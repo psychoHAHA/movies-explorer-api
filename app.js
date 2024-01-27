ccca103a68a9829e586b3f014da93c603170c144
kirill_zernovv <80909413+psychoHAHA@users.noreply.github.com>
2024-01-27 18:26:08 +0500

    Update app.js

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ const app = express()
 
 app.use(requestLogger)
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use(cors())
 
