2bead195670d278f8d5b6bda0eba1d9291ad01fd
kirill_zernovv <80909413+psychoHAHA@users.noreply.github.com>
2024-02-29 16:25:56 +0500

    123
    213

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const cors = require('./middlewares/cors');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
-const cors = require('cors');
 const router = require('./routes/index');
 const { errorLogger, requestLogger } = require('./middlewares/logger');
 const { errorHandle } = require('./middlewares/errorHandler');
@@ -13,14 +13,18 @@ const app = express();
 
 app.use(helmet());
 
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://psychodelic.movie.nomoredomainswork.ru'],
+  credentials: true,
+  maxAge: 30,
+}));
+
 app.use(requestLogger);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(router);
-
-app.use(cors());
 
 app.use(errors());
 
---
+++ middlewares/cors.js
@@ -0,0 +1,31 @@
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+const allowedCors = [
+  'http://localhost:3000',
+  'https://psychodelic.movie.nomoredomainswork.ru',
+  'http://psychodelic.movie.nomoredomainswork.ru',
+];
+
+module.exports = (req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+
+    res.header('Access-Control-Allow-Credentials', 'true');
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+
+    return res.end();
+  }
+
+  next();
+
+  return true;
+};
