d9943c483c2846295dc9aea7863621bc52885b07
kirill_zernovv <80909413+psychoHAHA@users.noreply.github.com>
2024-03-03 05:27:17 +0500

    Update app.js

--- app.js
+++ app.js
@@ -17,20 +17,22 @@ const { PORT = 3000, MONGO_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = proc
 
 const app = express();
 
+app.use(cors());
+
 app.use(helmet());
 
-const corsOptions = {
-  origin: '*',
-  credentials: true,
-  optionSuccessStatus: 200,
-};
+// const corsOptions = {
+//   origin: '*',
+//   credentials: true,
+//   optionSuccessStatus: 200,
+// };
 // app.use(cors({
 //   origin: ['http://localhost:5173', 'https://psychodelic.movie.nomoredomainswork.ru'],
 //   credentials: true,
 //   maxAge: 30,
 // }));
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 mongoose.connect(MONGO_URL);
 
