f54b8ac416b4006127d25cbb05289d8d38ade682
kirill_zernovv <80909413+psychoHAHA@users.noreply.github.com>
2024-04-21 00:46:41 +0500

    3333

--- controllers/movies.js
+++ controllers/movies.js
@@ -35,7 +35,7 @@ const createMovie = async (req, res, next) => {
 const deleteMovie = async (req, res, next) => {
   try {
     const userId = req.user._id // находим id юзера
-    const movieId = req.params.movieId // находим id фильма
+    const movieId = req.params // находим id фильма
     const findMovie = await movie // находим фильм с таким id
       .findById(movieId)
       .orFail(() => new ErrorNotFound('Фильм для удаления не найден'))
