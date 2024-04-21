03e7614407883607acc613da2168a56aeea007e9
kirill_zernovv <80909413+psychoHAHA@users.noreply.github.com>
2024-04-21 13:12:50 +0500

    Update movies.js

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,21 +1,22 @@
-const movie = require('../models/movie')
-const ErrorNotFound = require('../errors/errorNotFound')
-const ErrorValidation = require('../errors/errorValidation')
-const ErrorForbiden = require('../errors/errorForbidden')
+
+const movie = require('../models/movie');
+const ErrorNotFound = require('../errors/errorNotFound');
+const ErrorValidation = require('../errors/errorValidation');
+const ErrorForbiden = require('../errors/errorForbidden');
 
 const getMovies = async (req, res, next) => {
   try {
-    const movies = await movie.find({ owner: req.user._id })
+    const movies = await movie.find({ owner: req.user._id });
 
     if (!movies) {
-      throw new ErrorNotFound('Фильмы не были найдены')
+      throw new ErrorNotFound('Фильмы не были найдены');
     } else {
-      res.send(movies)
+      res.send(movies);
     }
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 const createMovie = async (req, res, next) => {
   try {
@@ -34,25 +35,26 @@ const createMovie = async (req, res, next) => {
 
 const deleteMovie = async (req, res, next) => {
   try {
-    const userId = req.user._id // находим id юзера
-    const movieId = req.params // находим id фильма
+    const userId = req.user._id; // находим id юзера
+    const newMovieId = req.params.movieId; // находим id фильма
     const findMovie = await movie // находим фильм с таким id
-      .findById(movieId)
-      .orFail(() => new ErrorNotFound('Фильм для удаления не найден'))
+      .findById(newMovieId)
+      .orFail(() => new ErrorNotFound('Фильм для удаления не найден'));
 
     if (!findMovie.owner.equals(userId)) {
-      throw new ErrorForbiden('Вы не можете удалить чужой фильм') // если владелец !== id юзера, то отправляем ошибку
+      throw new ErrorForbiden('Вы не можете удалить чужой фильм'); // если владелец !== id юзера, то отправляем ошибку
     } else {
-      const delMovie = await movie.deleteOne()
-      return res.send(delMovie) // если нашли удаляем ее и отправляем ответ об этом
+      const delMovie = await movie.deleteOne();
+      return res.send(delMovie); // если нашли удаляем ее и отправляем ответ об этом
     }
   } catch (error) {
-    next(error)
+    next(error);
+
   }
-}
+};
 
 module.exports = {
   getMovies,
   createMovie,
   deleteMovie,
-}
+};
